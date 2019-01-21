fa3af8568861704e0768a8ee95567e749eccedd7
TomShawley <thomas.shawley1@gmail.com>
2019-01-21 17:02:27 +0000

    Java EE added

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO TRAINEE (id, traineeID, traineeName ) VALUES (1, 'TS1', 'Thomas Shawley')
+INSERT INTO TRAINEE (id, traineeID, traineeName ) VALUES (2, 'AT1', 'Alex Thorpe')
+INSERT INTO TRAINEE (id, traineeID, traineeName ) VALUES (3, 'MH1', 'Matthew Hussey')
+
+INSERT INTO CLASSROOM (id, classroomID, trainer, trainees) VALUES (1, 1, 'Billy Bob', 'TS1')
+INSERT INTO CLASSROOM (id, classroomID, trainer, trainees) VALUES (2, 2, 'Bob Billy', 'AT1')
