ac48d549562181a8d3c9878bad633c3f1fa91797
TomShawley <thomas.shawley1@gmail.com>
2019-02-06 16:47:08 +0000

    Rule1 Added

--- src/main/java/com/qa/service/CharacterServiceImpl.java
+++ src/main/java/com/qa/service/CharacterServiceImpl.java
@@ -4,15 +4,23 @@ import javax.inject.Inject;
 
 
 import com.qa.persistence.repository.CharacterRepository;
+import com.qa.util.JSONUtil;
+import com.qa.persistence.domain.Character;
 
 
 public class CharacterServiceImpl implements CharacterService {
 	@Inject
 	private CharacterRepository repo;
 
-	
+	@Inject
+	JSONUtil util;
 
 	public String createCharacter(String character) {
+		Character newCharacter = util.getObjectForJSON(character, Character.class);
+		if (newCharacter.getName().matches("[a-zA-Z]+") != true) {
+			return "{\"message\": \"Character name can only contain letters\"}";
+			
+		}
 		return repo.createCharacter(character);
 	}
 
