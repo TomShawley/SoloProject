d5d5d417c07f614f28e503a008eba316a1058052
TomShawley <thomas.shawley1@gmail.com>
2019-02-07 13:43:41 +0000

    password verification fixed

--- src/main/java/com/qa/service/UserServiceImpl.java
+++ src/main/java/com/qa/service/UserServiceImpl.java
@@ -20,7 +20,7 @@ public class UserServiceImpl implements UserService {
 
 	public String createUser(String user) {
 		newUser = util.getObjectForJSON(user, User.class);
-		if (newUser.getPassword().matches("[a-zA-Z0-9]+") != true) {
+		if (newUser.getPassword().matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{3,}") == false) {
 			return "{\"message\": \"Password must contain atleat one Capital Letter and one Number.\"}";
 		} else if (newUser.getPassword().length() < 8 || newUser.getPassword().length() > 20) {
 			return "{\"message\": \"Password must be beween 8 and 20 characters.\"}";
