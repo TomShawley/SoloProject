eeeb0c8898f0fb742cabd64ee14c3bbdeebd438a
TomShawley <45848447+TomShawley@users.noreply.github.com>
2019-02-10 14:43:56 +0000

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 
 
 INSERT INTO USER (userid, username, password) VALUES (1, 'TomShawley', 'password')
-INSERT INTO USER (userid, username, password) VALUES (2, 'BadPlayer', 'shitpassword')
+INSERT INTO USER (userid, username, password) VALUES (2, 'BadPlayer', 'badpassword')
 
 INSERT INTO CHARACTER (id, name, race, characterClass, level, strength, dexterity, constitution, intelligence, wisdom, charisma, userid ) VALUES (1, 'Finn', 'Halfling', 'Bard', '7', '11', '16', '13', '15', '15', '20', 1)
 INSERT INTO CHARACTER (id, name, race, characterClass, level, strength, dexterity, constitution, intelligence, wisdom, charisma, userid ) VALUES (2, 'Twig Willow-Weed', 'Half-Elf', 'Druid', '3', '12', '15', '14', '13', '18', '10', 1)
 INSERT INTO CHARACTER (id, name, race, characterClass, level, strength, dexterity, constitution, intelligence, wisdom, charisma, userid ) VALUES (3, 'Jess Von-Eingard', 'Elf', 'Fighter', '5', '18', '11', '17', '12', '11', '8', 1)
-INSERT INTO CHARACTER (id, name, race, characterClass, level, strength, dexterity, constitution, intelligence, wisdom, charisma, userid ) VALUES (4, 'Shitty Druid Kid', 'Fircunt', 'Druid', '1', '1', '1', '1', '1', '1', '1', 2)
+INSERT INTO CHARACTER (id, name, race, characterClass, level, strength, dexterity, constitution, intelligence, wisdom, charisma, userid ) VALUES (4, 'Druid Kid', 'Firbolg', 'Druid', '1', '1', '1', '1', '1', '1', '1', 2)
